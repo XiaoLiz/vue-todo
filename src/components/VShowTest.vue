b25daabae6ae8932a791fe94d4db1ca79cb9e6d2
xiaolizi <xiaoliz@qq.com>
2022-03-17 10:23:14 +0800

    feat: init project

---
+++ src/components/VShowTest.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class="show-container">
+        v-if 与 v-show 触发机制
+    </div>
+</template>
+
+<script lang='ts'>
+export default {
+    name:'',
+    data () {
+        return {}
+    },
+
+    beforeCreate() {
+        console.log('beforeCreate')
+    },
+    created() {
+        console.log('created')
+    },
+
+    beforeMount() {
+        console.log('beforeMount')
+    },
+
+    mounted() {
+        console.log('mounted')
+    },
+
+    beforeUnmount() {
+        console.log('beforeDestroy')
+    },
+    unmounted() {
+        console.log('destroyed')
+    },
+}
+</script>
+
+<style lang='less' scoped>
+.show-container {
+    color:rgb(202, 121, 121);
+}
+</style>
